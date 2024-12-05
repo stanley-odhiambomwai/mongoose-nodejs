878aa9ee92d051c6f42036cb9eeaaf593f2767e2
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-05 12:16:06 +0300

    Operation with mongoose

--- Server.js
+++ Server.js
@@ -11,3 +11,23 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+const Person = require('./models/Person');
+
+const createPerson = async () => {
+    const person = new Person({
+        name: 'John Doe',
+        age: 30,
+        favoriteFoods: ['Pizza', 'Burger'],
+    });
+
+    try {
+        const savedPerson = await person.save();
+        console.log(savedPerson);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+createPerson();
